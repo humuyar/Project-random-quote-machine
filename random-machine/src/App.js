483aaf64a781ea8d16ed90eb2017c1720971110d
humuyar <humiq6071@gmail.com>
2024-08-21 22:04:03 -0700

    added some functionality for random qoute

--- random-machine/src/App.js
+++ random-machine/src/App.js
@@ -4,6 +4,34 @@ import COLORS_ARRAY from './colorsArray';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 
+const quoteDBUrl = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+
+function App() {
+  const [quote, setQuote] = useState ("I didn’t fail the test. I just found 100 ways to do it wrong.")
+  const [author, setAuthor] = useState ("Benjamin Franklin")
+  const [randomNumber, setRandomNumber] = useState(0)
+  const [quotesArray, setQuotesArray] = useState(null)
+  const [accentColor, setAccentColor] = useState('#6d366b9e')
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url)
+    const parsedJSON = await response.json()
+    setQuotesArray(parsedJSON.quotes)
+    console.log(parsedJSON)
+  }
+
+  useEffect(() => {
+    fetchQuotes(quoteDBUrl)
+  }, [quoteDBUrl])
+
+  const getRandomQuote = () => {
+    let randomInteger = Math.floor(quotesArray.length * Math.random());
+    setRandomNumber(randomInteger)
+    setAccentColor(COLORS_ARRAY[randomInteger])
+    setQuote(quotesArray[randomInteger].quote)
+    setAuthor(quotesArray[randomInteger].author)
+  }
+
 function App() {
   return (
     <div className="App">
