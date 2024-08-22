b8129c1bd9d07254e53d040c5c70cfe3df2dbefe
humuyar <humiq6071@gmail.com>
2024-08-21 22:03:02 -0700

    added some package in app file

--- random-machine/src/App.js
+++ random-machine/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import COLORS_ARRAY from './colorsArray';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
